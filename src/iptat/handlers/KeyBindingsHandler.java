7f71c489880d0868d7492db6c032a2bcdf56a9e7
Alexandru Ghergut <alexandru.ghergut94@gmail.com>
2016-01-14 12:47:50 +0200

    Refactor KeyBindingsHandler and add bindings for zoom in/out

--- src/iptat/handlers/KeyBindingsHandler.java
+++ src/iptat/handlers/KeyBindingsHandler.java
@@ -1,6 +1,7 @@
 package iptat.handlers;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
 import java.util.Scanner;
 
 import javax.swing.AbstractAction;
@@ -10,6 +11,8 @@ import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+
+import com.sun.glass.events.KeyEvent;
 
 import iptat.gui.DrawingBoard;
 import iptat.util.EarCutter;
@@ -25,58 +28,14 @@ public class KeyBindingsHandler {
 		InputMap inputMap = drawingBoard.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
 		ActionMap actionMap = drawingBoard.getActionMap();
 		
-		Action reset = new AbstractAction() {
-			public void actionPerformed(ActionEvent e) {
-				drawingBoard.setTranslateX(0);
-				drawingBoard.setTranslateY(0);
-				drawingBoard.setScaleX(1);
-				drawingBoard.setScaleY(1);
-				
-				drawingBoard.getPolygon().clear();
-			}
-		};
-		
-		inputMap.put(KeyStroke.getKeyStroke("R"), "reset");
-		actionMap.put("reset", reset);
-		
-		Action undo = new AbstractAction() {
-			public void actionPerformed(ActionEvent e) {
-				drawingBoard.getPolygon().removeLast();
-			}
-		};
-		
-		inputMap.put(KeyStroke.getKeyStroke("control Z"), "undo");
-		actionMap.put("undo", undo);
-		
-		Action redo = new AbstractAction() {
-			public void actionPerformed(ActionEvent e) {
-				drawingBoard.getPolygon().restoreLast();
-			}
-		};
-		
-		inputMap.put(KeyStroke.getKeyStroke("control X"), "redo");
-		actionMap.put("redo", redo);
-		
-		Action triangulate = new AbstractAction() {
-			public void actionPerformed(ActionEvent e) {
-				Polygon2D polygon = drawingBoard.getPolygon();
-				
-				if (!polygon.isTriangulated() && !polygon.isTriangulating()) {
-					TriangulationHandler th = new TriangulationHandler(new EarCutter());
-					
-					try {
-						th.triangulate(polygon);
-						polygon.setTriangulationInProgress();
-					} catch (Exception exc) {
-						JOptionPane.showMessageDialog(null, exc.getMessage(), 
-								"Unable to start triangulation!", JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}
-		};
-		
-		inputMap.put(KeyStroke.getKeyStroke("T"), "triangulate");
-		actionMap.put("triangulate", triangulate);
+		initFileBindings(drawingBoard, actionMap, inputMap);
+		initEditBindings(drawingBoard, actionMap, inputMap);
+		initTriangulationBindings(drawingBoard, actionMap, inputMap);
+		affineTransformBindings(drawingBoard, actionMap, inputMap);
+	}
+	
+	private static void initFileBindings(DrawingBoard drawingBoard, 
+			ActionMap actionMap, InputMap inputMap) {
 		
 		Action save = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
@@ -95,6 +54,10 @@ public class KeyBindingsHandler {
 		
 		inputMap.put(KeyStroke.getKeyStroke("control O"), "load");
 		actionMap.put("load", load);
+	}
+	
+	private static void initEditBindings(DrawingBoard drawingBoard, 
+			ActionMap actionMap, InputMap inputMap) {
 		
 		Action addPoints = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
@@ -121,5 +84,86 @@ public class KeyBindingsHandler {
 		
 		inputMap.put(KeyStroke.getKeyStroke("control A"), "addPoints");
 		actionMap.put("addPoints", addPoints);
+		
+		Action undo = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				drawingBoard.getPolygon().removeLast();
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke("control Z"), "undo");
+		actionMap.put("undo", undo);
+		
+		Action redo = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				drawingBoard.getPolygon().restoreLast();
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke("control X"), "redo");
+		actionMap.put("redo", redo);
+		
+		Action reset = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				drawingBoard.setTranslateX(0);
+				drawingBoard.setTranslateY(0);
+				drawingBoard.setScaleX(1);
+				drawingBoard.setScaleY(1);
+				
+				drawingBoard.getPolygon().clear();
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke("R"), "reset");
+		actionMap.put("reset", reset);
+	}
+	
+	private static void initTriangulationBindings(DrawingBoard drawingBoard, 
+			ActionMap actionMap, InputMap inputMap) {
+		
+		Action triangulate = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				Polygon2D polygon = drawingBoard.getPolygon();
+
+				if (!polygon.isTriangulated() && !polygon.isTriangulating()) {
+					TriangulationHandler th = new TriangulationHandler(new EarCutter());
+
+					try {
+						th.triangulate(polygon);
+						polygon.setTriangulationInProgress();
+					} catch (Exception exc) {
+						JOptionPane.showMessageDialog(null, exc.getMessage(), 
+								"Unable to start triangulation!", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke("T"), "triangulate");
+		actionMap.put("triangulate", triangulate);
+	}
+	
+	private static void affineTransformBindings(DrawingBoard drawingBoard,
+			ActionMap actionMap, InputMap inputMap) {
+		
+		Action zoomIn = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				drawingBoard.incrementScale();
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ADD, InputEvent.CTRL_DOWN_MASK), "zoomIn");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, InputEvent.CTRL_DOWN_MASK), "zoomIn");
+		actionMap.put("zoomIn", zoomIn);
+		
+		Action zoomOut = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				drawingBoard.decrementScale();
+			}
+		};
+		
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT, InputEvent.CTRL_DOWN_MASK), "zoomOut");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, InputEvent.CTRL_DOWN_MASK), "zoomOut");
+		actionMap.put("zoomOut", zoomOut);
 	}
 }
